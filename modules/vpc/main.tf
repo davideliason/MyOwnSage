ba4189a0d654c5a682d2d589b7bf486885221518
David Eliason <davethemaker@gmail.com>
2025-02-27 01:17:50 -0800

    add public subnet using cidrsubnet function, with 4 and 1 arguments pointing to the mask length and the subnet number

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,7 +1,17 @@
-resource "aws_vpc" "main-vp" {
-  cidr_block             = var.cidr_block
-  enable_dns_hostnames   = var.enable_dns_hostnames
-  enable_dns_support    = var.enable_dns_support
+resource "aws_vpc" "main-vpc" {
+  cidr_block                 = var.cidr_block
+  enable_dns_hostnames       = var.enable_dns_hostnames
+  enable_dns_support         = var.enable_dns_support
 
   tags = var.tags
+}
+
+ resource "aws_subnet" "public_subnet-1" {
+  vpc_id                     = aws_vpc.main-vpc.id
+  cidr_block                 = cidrsubnet(aws_vpc.main-vpc.cidr_block, 4,1)
+  availability_zone          = "us-west-2a"
+
+  tags = {
+    Name = "MoS-public-SN-us-west-2a"
+  }
 }
