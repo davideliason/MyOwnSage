33da21d9b5f928fab51d32efc21a5cd18a00a7ad
David Eliason <davethemaker@gmail.com>
2025-02-26 19:24:26 -0800

    successful creation of vpc, enable dns_hostnames, enable_dns_support, with tags

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "cidr_block" {
+  description       = "The default CIDR block for the VPC"
+  type              = string
+  default           = "10.16.0.0/16"
+}
+
+variable "enable_dns_hostnames" {
+  description       = "A boolean flag to enable/disable DNS hostnames in the VPC"
+  type              = bool
+  default           = true
+}
+
+variable "enable_dns_support" {
+  description       = "A boolean flag to enable/disable DNS support in the VPC"
+  type              = bool
+  default           = true
+}
+
+variable "tags" {
+  description = "A map of tags to add to the VPC"
+  type        = map(string)
+  default     = { 
+     Name = "MyOwnSage"
+ }
+}
+
+
