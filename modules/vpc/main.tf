a42a53edeaca70b9324fae240929bca59aaa29d3
David Eliason <davethemaker@gmail.com>
2025-02-27 01:29:53 -0800

    add private subnet within first AZ and IGW

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -15,3 +15,21 @@ resource "aws_vpc" "main-vpc" {
     Name = "MoS-public-SN-us-west-2a"
   }
 }
+
+resource "aws_subnet" "private_subnet-1" {
+  vpc_id                    = aws_vpc.main-vpc.id
+  cidr_block                = cidrsubnet(aws_vpc.main-vpc.cidr_block, 4,2)
+  availability_zone         = "us-west-2a"
+
+  tags = {
+    Name = "MoS-private-SN-us-west-2a"
+  }
+}
+
+resource "aws_internet_gateway" "MoS-IGW" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  tags = {
+    Name = "Mos-main-VPC-IGW"
+  }
+}
